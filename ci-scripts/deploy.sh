3d61657a1dc0fdd64449b92533c35a4bd17f2a85
Nick <nick@wirestorm.us>
2017-04-26 10:24:11 -0700

    updated deploy script

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -10,5 +10,6 @@ docker push ${DOCKERHUB_REPO}:$(get_fulldockertag)
 if [[ "$TRAVIS_BRANCH" == "master" ]] 
 	then 
 	echo "... as master"
+	docker tag makeen ${DOCKERHUB_REPO}:latest
 	docker push ${DOCKERHUB_REPO}:latest
 fi
